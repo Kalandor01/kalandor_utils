ae25e20be4cefadae30c8bd108efb2f55aa10d4f
Kalandor01 <rohovszkyakoska@gmail.com>
2022-05-03 09:08:38 +0200

    actual hell

--- kalandor_utils/__init__.py
+++ kalandor_utils/__init__.py
@@ -32,3 +32,73 @@ def _timed_input(text="Quick: ", time=3000):
     if return_value == -1:
         print("\n")
     return return_value
+
+
+# _timed_input()
+
+def thread_test():
+    from time import sleep
+    import concurrent.futures as future
+
+    def func_input(text:str):
+        ans = -1
+        ans = input(text)
+        return ans
+    
+    def func_timer(time:int=3, text:str="ask: "):
+        with future.ThreadPoolExecutor() as th:
+            th_input = th.submit(func_input, text)
+            sleep(time)
+            print("STOP")
+            return_ans = th_input.result()
+            resoult = return_ans
+            return resoult
+
+    with future.ThreadPoolExecutor() as th:
+        th_1 = th.submit(func_timer, 5, "question: ")
+        sleep(3)
+        print("STOP")
+        return_val = th_1.result()
+        print(return_val)
+
+# thread_test()
+
+
+
+
+
+
+
+
+# from threading import Thread
+# import time
+# import os
+
+# answer = None
+
+
+# def ask():
+#     global start_time, answer
+#     start_time = time.time()
+#     answer = input("Enter a number:\n")
+#     time.sleep(0.001)
+
+
+# def timing():
+#     time_limit = 5
+#     while True:
+#         time_taken = time.time() - start_time
+#         if answer is not None:
+#             print(f"You took {time_taken} seconds to enter a number.")
+#             os._exit(1)
+#         if time_taken > time_limit:
+#             print("Time's up !!! \n"
+#                   f"You took {time_taken} seconds.")
+#             os._exit(1)
+#         time.sleep(0.001)
+
+
+# t1 = Thread(target=ask)
+# t2 = Thread(target=timing)
+# t1.start()
+# t2.start()
